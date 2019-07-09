1345dc991d92be83a77ff5d915f5c35be9124742
Suzanne Smigielski <ssmigiel571@gmail.com>
2019-07-09 15:45:44 -0500

    foundation code

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
 
-const apiKey = 'v7M0R5aZGFl5syUwiKzEdcsroABEQcPveWnWC8R0';
+    function searchParks(){
 
+        const apiKey = 'v7M0R5aZGFl5syUwiKzEdcsroABEQcPveWnWC8R0';
+        let apiURL = 
+        let apiPath = 
+
+        fetch(apiPath)
+        .then(status)
+        .then(response=>response.json())
+        .then(responseJson=>
+                displayResults(responseJson())
+    }
+
+    function status(){
+        if(!response.ok){
+            throw alert("Oops. Something went wrong!")
+        }
+        return response;
+    }
+
+    function displayResults(){
+        
+    }
 
 
 
